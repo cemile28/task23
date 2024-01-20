cc4cb5ffb025214bf9ce3be31e83024aeea87c5d
Cemile Kamilova <146671502+cemile28@users.noreply.github.com>
2024-01-20 17:43:18 +0400

    Add files via upload

---
+++ cemi.js
@@ -0,0 +1,112 @@
+//task 1
+
+//const user1 = {
+
+  //  name : "Jamila",
+   // gender : "Female",
+   // birthday : "28.04.2003"
+
+
+//}
+
+//task 2
+
+//user.age = "20"
+//delete user.birthday
+
+
+//task 3
+//setAge: function (newAge){
+    // age : "20",
+   //this.age = newAge
+
+  // user.setAge(27)
+   //console.log(user)
+
+
+   //getYearsBeforeRetirement: function (){
+    // return 65 - this.age
+    // console.log(user.getYearsBeforeRetirement())
+    
+    
+  // }
+//}
+
+// task 4
+
+
+//constructor(fullName, creditLimit) {
+ // this.fullName = fullName;
+ // this.creditLimit = creditLimit;
+ // this.balans = 0;
+ // this.precentOfMinPayment = 5;
+ // }
+  
+  //getBalance() {
+  //if (this.balans < 0) {
+  //return Borcunuz ${-this.balans};
+  //} else {
+  //return Balansınız ${this.balans};
+  //}
+  //}
+  
+ // getMinPaymant() {
+  //if (this.balans > 0) {
+ // let minPayment = this.balans * (this.precentOfMinPayment / 100);
+  //return Minimum ödənişiniz ${minPayment};
+  //} else {
+  //return Sizin borcunuz yoxdur;
+  //}
+  //}
+  
+  //withdraw(amount) {
+  //if (this.balans >= amount) {
+  //this.balans -= amount;
+ // console.log(Balansınızdan ${amount} manat silindi. Cari balansınız ${this.balans});
+ // } else if (this.balans + this.creditLimit >= amount) {
+ // console.log(Kredit limitinizdən ${amount - this.balans} manat istifadə edildi);
+ // this.creditLimit -= (amount - this.balans);
+ // this.balans = 0;
+ // } else {
+ // console.log("Kifayət qədər vəsaitiniz yoxdur");
+ // }
+ // }
+  
+ // refill(amount) {
+ // this.balans += amount;
+ // console.log(Balansınıza ${amount} manat əlavə edildi. Cari balansınız ${this.balans});
+ // }
+ // }
+ 
+  // Example usage
+ // let client = new Client("John Doe", 1000);
+  //console.log(client.getBalance());
+  //console.log(client.getMinPaymant());
+  //client.withdraw(900);
+  //client.refill(500);
+
+
+  // task 5
+
+ // calculator = Calculator()
+
+//result = calculator.add(5, 3)
+//print(result)  # Output: 8
+//print(calculator.get_memory())  # Output: 8
+
+//result = calculator.subtract(7, 2)
+//print(result)  # Output: 5
+//print(calculator.get_memory())  # Output: 5
+
+//calculator.save_memory(10)
+//print(calculator.get_memory())  # Output: 10
+
+//calculator.clear_memory()
+//print(calculator.get_memory())  # Output: None
+  
+  
+  
+  
+  
+  
+
